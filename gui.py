86235419351cc04eee8b7b1928627ee15d20963c
Lorenz <LorenzRenti@outlook.com>
2023-05-14 15:44:36 +0200

    v1.0

--- gui.py
+++ gui.py
@@ -1,44 +1,55 @@
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import messagebox
 from deck_builder import DeckBuilder
 from utils import get_version_number
+import webbrowser
 
 class AnkiPackBuilderGUI:
     def __init__(self):
         self.window = tk.Tk()
         self.window.title("Anki Pack Builder")
-        self.window.geometry("400x200")
+        self.window.geometry("350x200")
+        self.window.resizable(False, False)
+        self.window.configure(bg="#202020")
 
-        version_label = tk.Label(self.window, text="Version: " + get_version_number())
-        version_label.place(x=10, y=180)
+        self.version_label = tk.Label(self.window, text="Version: " + get_version_number(), bg="#202020", fg="white")
+        self.version_label.place(x=10, y=180)
 
-        developer_label = tk.Label(self.window, text="by LRenTi")
-        developer_label.place(x=310, y=180)
+        self.developer_label = tk.Label(self.window, text="by LRenTi", bg="#202020", fg="white")
+        self.developer_label.place(x=290, y=180)
 
         self.input_file_path = None
         self.deck_name = None
 
-        self.input_file_label = tk.Label(self.window, text="Input File:")
-        self.input_file_label.pack()
-
-        self.input_file_button = tk.Button(self.window, text="Select Input File", command=self.select_input_file)
-        self.input_file_button.pack()
-
-        self.deck_name_label = tk.Label(self.window, text="Deck Name:")
-        self.deck_name_label.pack()
+        self.deck_name_label = tk.Label(self.window, text="Deck Name:", font=("Helvetica", 12, "bold"), bg="#202020", fg="white")
+        self.deck_name_label.place(x=10, y=20)
 
         self.deck_name_entry = tk.Entry(self.window)
-        self.deck_name_entry.pack()
+        self.deck_name_entry.place(x=125, y=25)
 
-        self.generate_button = tk.Button(self.window, text="Generate Anki Deck", command=self.generate_anki_deck)
-        self.generate_button.pack()
+        self.input_file_label = tk.Label(self.window, text="Input File:", font=("Helvetica", 12, "bold"), bg="#202020", fg="white")
+        self.input_file_label.place(x=10, y=70)
 
-        self.success_label = tk.Label(self.window, text="")
+        self.input_file_button = tk.Button(self.window, text="Select Input File", command=self.select_input_file, bg="#3498db", fg="white", relief="flat", padx=10, borderwidth=0, cursor="hand2")
+        self.input_file_button.place(x=125, y=70)
+
+        self.selected_file_label = tk.Label(self.window, text="", font=("Helvetica", 8), bg="#202020", fg="white")
+        self.selected_file_label.place(x=240, y=72)
+
+        self.generate_button = tk.Button(self.window, text="Create Anki Deck", command=self.generate_anki_deck, bg="#3498db", fg="white", relief="flat", padx=10, pady=5, borderwidth=0, cursor="hand2")
+        self.generate_button.place(x=10, y=125)
+
+        self.help_button = tk.Button(self.window, text="?", command=self.open_help, font=("Helvetica", 9, "bold"), bg="#202020", fg="white", relief="flat", width=2, cursor="hand2")
+        self.help_button.place(x=320, y=10)
+
+        self.success_label = tk.Label(self.window, text="",bg="#202020", fg="white")
         self.success_label.pack()
+        self.success_label.place(x=135, y=130)
 
     def select_input_file(self):
         self.input_file_path = filedialog.askopenfilename(filetypes=[('Text Files', '*.txt')], title="Select Input File")
-        self.input_file_label.config(text=f"Input File: {self.get_file_name(self.input_file_path)}")
+        self.selected_file_label.config(text=self.get_file_name(self.input_file_path))
 
     def get_file_name(self, file_path):
         if file_path:
@@ -47,8 +58,12 @@ class AnkiPackBuilderGUI:
 
     def generate_anki_deck(self):
         self.deck_name = self.deck_name_entry.get()
-        DeckBuilder(self.input_file_path, self.deck_name).generate_anki_deck()
-        self.success_label.config(text="Anki deck successfully generated!")
+        if self.deck_name and self.input_file_path:
+            DeckBuilder(self.input_file_path, self.deck_name).generate_anki_deck()
+            self.success_label.config(text="Anki deck successfully generated!")
+
+    def open_help(self):
+        webbrowser.open("https://github.com/LRenTi/AnkiPackBuilder/")
 
     def start(self):
         self.window.mainloop()
--- utils.py
+++ utils.py
@@ -1,19 +1,12 @@
 from tkinter import filedialog
+import tkinter as tk
 import tkinter.messagebox as messagebox
 import requests
-
-class FileManager:
-    @staticmethod
-    def select_input_file():
-        return filedialog.askopenfilename(filetypes=[('Text Files', '*.txt')], title="Select Input File")
-
-    @staticmethod
-    def save_output_file():
-        return filedialog.asksaveasfilename(defaultextension=".apkg", filetypes=[('Anki Deck Files', '*.apkg')],
-                                            title="Save Anki Deck")
+from tkinter import messagebox, Tk, Label, Button
+import webbrowser
 
 def get_version_number():
-    return "v0.1"  # Replace with your actual version number
+    return "v1.0"  # Replace with your actual version number
 
 def check_for_update():
     current_version = get_version_number()
@@ -21,4 +14,20 @@ def check_for_update():
     latest_version = response.json()["tag_name"]
 
     if latest_version != current_version:
-        messagebox.showinfo("Update Available", f"A new version ({latest_version}) of Anki Pack Builder is available. Please update to the latest version.")
+        root = Tk()
+        root.title("Update Available")
+        root.geometry("300x120")
+        root.resizable(False, False)
+        root.configure(bg="#202020")
+
+        message_label = Label(root, text=f"A new version ({latest_version}) of Anki Pack Builder is available.\n Please update to the latest version.", font=("Helvetica", 9), bg="#202020", fg="white")
+        message_label.pack(pady=10)
+
+        download_button = Button(root, text="Download", command=lambda: open_link("https://github.com/LRenTi/AnkiPackBuilder/releases/latest"))
+        download_button.pack(pady=10)
+
+        root.mainloop()
+
+def open_link(url):
+    webbrowser.open(url)
+    pass
